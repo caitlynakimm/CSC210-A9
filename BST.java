6e4ea2a10b9375ff3c2cdcb300b0c333c00af6fc
Caitlyn Kim <cakim@smith.edu>
2025-11-17 17:06:04 -0500

    working on implementing BST methods

--- BST.java
+++ BST.java
@@ -1,8 +1,93 @@
 /**
  * Implements binary search trees.
  *
- * @author YOUR_NAME_HERE
+ * @author Caitlyn Kim
  * @version Fall 2025
  */
 public class BST<E extends Comparable<E>> extends BinaryTree<E> implements BST_Ops<E> {
+    
+    /** This constructor creates a leaf node */
+    public BST(E data) {
+        super(data);
+    }
+    
+    /**
+     * Creates branch node with left and right children
+     * @param data question for this node
+     * @param left left child of node
+     * @param right right child of node
+     */
+    public BST(E data, BinaryTree<E> left, BinaryTree<E> right) {
+        super(data);
+        this.setLeft(left); //checks that left is type BST
+        this.setRight(right); //checks that right is type BST
+    }
+
+    /** This constructor creates a deep copy of the entire tree structure */ 
+    public BST(BinaryTree<E> tree) {
+        super(tree.getData());
+        this.setLeft(tree.getLeft());
+        this.setRight(tree.getRight());
+    }
+
+    /** Override inherited manipulator to accept only BST */
+    public void setLeft(BinaryTree<E> left) {
+        if ((left==null)||(left instanceof BST<E>)) {
+            super.setLeft(left);
+        } else {
+            throw new UnsupportedOperationException("Only BST children allowed");
+        }
+    }
+
+    /** Override inherited manipulator to accept only BST */
+    public void setRight(BinaryTree<E> right) {
+        if ((right==null)||(right instanceof BST<E>)) {
+            super.setLeft(right);
+        } else {
+            throw new UnsupportedOperationException("Only BST children allowed");
+        }
+    }
+
+    /**
+     *  Returns the node of the given element, or null if not found
+     *
+     *  @param query The element to search
+     *  @return the node of the given element, or null if not found
+     */
+    public BST<E> lookup(E data){}
+
+    /**
+     *  Inserts a new node into the tree
+     *
+     *  @param data The element to insert
+     */
+    public void insert(E data);
+
+    /**
+     *  Deletes the specified element from the tree
+     *  Returns the modified tree because the root node 
+     *  may have changed
+     *  
+     *  @param evictee The element to delete
+     *  @return tree as modified
+     */
+    public BST<E> deleteWithCopyLeft(E evictee);
+
+    /**
+     *  Apply left rotation
+     *  Returns the modified tree because the root node 
+     *  may have changed
+     *
+     *  @return tree as modified
+     */
+    public BST<E> rotateLeft();
+
+    /**
+     *  Apply right rotation
+     *  Returns the modified tree because the root node 
+     *  may have changed
+     *
+     *  @return tree as modified
+     */
+    public BST<E> rotateRight();
 }
